c1b4830124ac4fe489ea8895159b1809d2f9e40d
Jeremiah Miller <j_miller429@yahoo.com>
2017-03-22 14:59:31 -0400

    added C#

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool elevatorUp = true;
+            bool elevatorDown = true;
+            bool elevatorBroken = true;
+            bool elevatorStuckWhileWeAreInIt = true;
+            int elevatorNumber = 13;
+
+            if (elevatorUp == true)
+            {
+                //DO stuff
+                Console.WriteLine("Going up");
+            }
+            else
+            {
+                //Do something else
+                Console.WriteLine("Going down");
+            }
+            if (elevatorBroken)
+            {
+                Console.WriteLine("Panic!!!");
+            }
+            else
+            {
+                Console.WriteLine("Have a Good Day");
+            }
+            if (elevatorStuckWhileWeAreInIt && elevatorDown)
+            {
+                Console.WriteLine("Let's get rescued");
+                Console.WriteLine("Yeah");
+            }
+            else
+            {
+                Console.WriteLine("Yea!");
+            }
+            if (elevatorNumber == 13)
+            {
+                Console.WriteLine("I'm scared we are going to suffocat");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bankAccount = 50000;
+            int debt = 13500;
+            int difference = bankAccount - debt;
+
+            Console.WriteLine("I have {0} in my bank acount, and I am {1} in debt", bankAccount, debt);
+            if (difference >= 7000 && difference <= 1999)
+            {
+                Console.WriteLine("I have extra money to buy socks this month");
+            }
+            else if ((bankAccount -debt > 2000))
+            {
+                Console.WriteLine("I have a bunch of extra money");
+            }
+            else
+            {
+                Console.WriteLine("It isn't a good time to pay off debt");
+            }
+            Console.ReadLine();
+    }
+}
