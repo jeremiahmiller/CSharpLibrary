263646f367ee3c203931802a8013286e7c77e1bb
Jeremiah Miller <j_miller429@yahoo.com>
2017-03-22 10:32:04 -0400

    adding more

---
+++ 0.01_Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _0._01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
