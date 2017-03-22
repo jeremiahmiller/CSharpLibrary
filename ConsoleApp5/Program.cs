35d8cb508610d74736a0c3a7293f6f4ce3dad668
Jeremiah Miller <j_miller429@yahoo.com>
2017-03-22 13:40:25 -0400

    added more

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOn = true;
+            bool isHot = false;
+
+            bool isAdmin = true;
+            bool isLoggedIn = true;
+            
+            if (isAdmin)
+            {
+                Console.WriteLine("Welcome to the site Admin");
+            }
+
+            if (isLoggedIn)
+            {
+                Console.WriteLine("You're logged in as an administrator");
+            }
+
+            if (isOn)
+            {
+                Console.WriteLine("The light is on.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
